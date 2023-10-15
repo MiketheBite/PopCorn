162ca52cc370f854aa8f260b02a01952f651d1dd
MiketheBite <migueljcpinto@mail.com>
2023-10-15 23:12:29 +0200

    security errors

--- components/Star/Star.jsx
+++ components/Star/Star.jsx
@@ -19,7 +19,7 @@ export default function Star({
     >
       {full ? (
         <svg
-          xmlns="http://www.w3.org/2000/svg"
+          xmlns="https://www.w3.org/2000/svg"
           viewBox="0 0 20 20"
           fill={color}
           stroke={color}
@@ -28,7 +28,7 @@ export default function Star({
         </svg>
       ) : (
         <svg
-          xmlns="http://www.w3.org/2000/svg"
+          xmlns="https://www.w3.org/2000/svg"
           fill="none"
           viewBox="0 0 24 24"
           stroke={color}
